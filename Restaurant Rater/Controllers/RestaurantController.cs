fd62b7a405690ce764af8f03b745a0054750d0b8
Arthur <arthurrobinsoniii3@gmail.com>
2020-03-23 16:11:04 -0400

    modified the edit controller and view

--- Restaurant Rater/Controllers/RestaurantController.cs
+++ Restaurant Rater/Controllers/RestaurantController.cs
@@ -1,6 +1,7 @@
 ﻿using Restaurant_Rater.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Net;
 using System.Web;
@@ -59,6 +60,37 @@ namespace Restaurant_Rater.Controllers
             _db.SaveChanges();
             return RedirectToAction("index");
 
+        }
+
+        public ActionResult Edit(int? id)
+        {
+            {
+                if (id == null)
+                {
+                    return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+                }
+
+                Restaurant restaurant = _db.Restaurants.Find(id);
+
+                if (restaurant == null)
+                {
+                    return HttpNotFound();
+                }
+
+                return View(restaurant);
+            }
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Restaurant restaurant)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Entry(restaurant).State = EntityState.Modified;
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(restaurant);
         }
     }
 }
